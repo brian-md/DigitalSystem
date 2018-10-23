e6800beb6c576d47a3a8cbd3b9fa63841769a70c
Michael Thariath <Mike@localhost.localdomain>
2018-10-22 20:41:26 -0500

    continued footer

--- src/components/Footer/Footer.css.js
+++ src/components/Footer/Footer.css.js
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
   grid-template-columns: 1fr 2fr 1fr;
   grid-template-rows: 4rem 1fr min-content;
   grid-template-areas:
-    'siteInfo social contact'
+    'social social social'
     'siteInfo nav contact'
     'copyright copyright copyright';
   font-weight: 100;
@@ -14,24 +14,51 @@ export const Wrapper = styled.div`
 
 export const SiteInfo = styled.div`
   grid-area: siteInfo;
+  p:only-of-type {
+    margin-top: 0.5rem;
+    font-size: 0.9rem;
+    line-height: 1.2rem;
+    opacity: 0.9;
+    margin-bottom: 1rem;
+  }
+  address {
+    font-size: 0.8rem;
+  }
 `;
 
 export const Social = styled.ul`
   grid-area: social;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: center;
   align-content: center;
+  > * {
+    margin: 0 1rem;
+  }
 `;
 
 export const Contact = styled.div`
   grid-area: contact;
+  > div {
+    margin-bottom: 1rem;
+  }
   span {
     text-align: left;
   }
 `;
 
-export const Nav = styled.dl`
+export const Nav = styled.nav`
   grid-area: nav;
+  font-size: 1rem;
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const NavList = styled.div`
+  h3 {
+    text-transform: uppercase;
+    font-weight: 900;
+    margin-bottom: 0.5rem;
+  }
 `;
 
 export const Copyright = styled.div`
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,8 +6,9 @@ import {
   Contact,
   Nav,
   Copyright,
+  NavList,
 } from './Footer.css';
-import { Section, Logo, IconButton, IconTitle } from 'components';
+import { Section, Logo, IconButton, Paragraph, IconTitle } from 'components';
 
 const Footer = () => (
   <Section
@@ -31,9 +32,42 @@ const Footer = () => (
           info@digitalsystemsav.com
         </IconTitle>
       </Contact>
-      <Nav>nav</Nav>
+      <Nav>
+        <NavList>
+          <h3>Website</h3>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Support</li>
+            <li>Contact</li>
+          </ul>
+        </NavList>
+        <NavList>
+          <h3>Services</h3>
+          <ul>
+            <li>Contol4</li>
+            <li>Digital Signage</li>
+            <li>Audio</li>
+            <li>Networking</li>
+          </ul>
+        </NavList>
+        <NavList>
+          <h3>Industries</h3>
+          <ul>
+            <li>Contol4</li>
+            <li>Digital Signage</li>
+            <li>Audio</li>
+            <li>Networking</li>
+          </ul>
+        </NavList>
+      </Nav>
       <SiteInfo>
         <Logo color="#ffffff" height={5} />
+        <Paragraph>
+          We design, install, and service audio-video and other technology
+          systems for homes and businesses in the Chicagoland area.
+        </Paragraph>
+        <address>2545 W Diversey Pkwy Suite 205 Chicago, IL 60647</address>
       </SiteInfo>
       <Copyright>&copy; 2018 Digital Systems and Integration, Inc. </Copyright>
     </Wrapper>
