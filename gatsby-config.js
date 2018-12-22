d4fcc1e1765b2c55ed392ca823e75447e89e775a
Michael Thariath <mictha1@gmail.com>
2018-12-21 22:48:32 -0600

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,5 +82,25 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-131228110-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Enables Google Optimize using your container Id
+        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'digitalsystemsav.com',
+      },
+    },
   ],
 };
